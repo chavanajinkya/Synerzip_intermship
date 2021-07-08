a9cd82e52ea34a5cda5640248dd9c58b73d4660a
Ajinkya Chavan <64342938+chavanajinkya@users.noreply.github.com>
2021-07-08 14:21:53 +0530

    Add files via upload

---
+++ synerzip/xpath.js
@@ -0,0 +1,27 @@
+//  XPath //
+//  Xpath is an address of a html element present inside the DOM//
+
+
+
+// Xpath syntax  //
+
+
+//htmltag[@attribute = 'value']      most preferred attributes id,class,name
+
+
+// Xpath syntax 2 //
+
+
+//htmltag[@attribute = 'value' and @attribute = 'value' and @attribute = 'value']        //here attribute can be 3 or 2 or 1 
+
+
+
+
+//uses of xpath 
+
+
+//we can access the attibutes and values of the hidden elements in DOM
+// for example we can access the properties of element after the signin page even if we have not signed in
+
+
+
