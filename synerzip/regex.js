967b8f2fba9a8e67d0e8c6e2f20c0e547fde6e4f
Ajinkya Chavan <Ajinkya.Chavan@datametica.com>
2022-12-08 17:17:28 +0530

    recent update

--- synerzip/regex.js
+++ synerzip/regex.js
@@ -1,1 +1,13 @@
-vbnhvb
+--- 
+
+REGEX 
+
+
+
+Regex is an alternative way to get the desired elements from the text 
+
+This is a very huge concept but you can learn it by doing it at 
+www.regexr.com
+
+
+------
